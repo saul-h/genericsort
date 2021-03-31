07d7c1f899508bf0d5f17f00579c11e8925d9066
saulh1 <Saulhernandez217@gmail.com>
2021-03-31 06:51:33 +0000

    Added data

--- genericsort.cs
+++ genericsort.cs
@@ -1,1 +1,17 @@
+using System
+Class Person
+{
+  public string Name { get; set; }
+  public int Age { get; set; }
+}
 
+public static void Main()
+{
+  Person[] people = { new Person { Name = Hal, Age = 20 },
+                      new Person { Name = Susann, Age = 31 },
+                      new Person { Name = Dwight, Age = 19 }
+  }
+  double[] numbers = { 645.41, 37.59, 76.41, 5.31, -34.23, 1.11, 1.10, 23.46, 635.47, -876.32, 467.83, 62.25 };
+
+  Console.WriteLine("Hello");
+}
